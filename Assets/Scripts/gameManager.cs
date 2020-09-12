4a37c717bc0ab4a6f08e1920ebbefd8096c03d5a
AhsanAAR <39828020+AhsanAAR@users.noreply.github.com>
2020-09-12 15:22:24 +0500

    explosion added

---
+++ Assets/Scripts/gameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class gameManager : MonoBehaviour
+{
+    public GameObject ball;
+    bool start;
+    public GameObject explosion;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        start = false;
+ //       ball.GetComponent<Rigidbody2D>().isKinematic = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 touchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            if (!start)
+            {
+                start = true;
+                ball.GetComponent<Rigidbody2D>().isKinematic = false;
+            }
+
+            Destroy(Instantiate(explosion,touchPos, Quaternion.identity),0.3f);
+
+        }
+    }
+}
