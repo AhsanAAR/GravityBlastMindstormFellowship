d8f40d827453f1bd182ae09db7686c21b4e15d5e
AhsanAAR <39828020+AhsanAAR@users.noreply.github.com>
2020-10-05 21:46:14 +0500

    side walls added

--- Assets/Scripts/cameraControl.cs
+++ Assets/Scripts/cameraControl.cs
@@ -9,9 +9,11 @@ public class cameraControl : MonoBehaviour
     public GameObject ball;
     public GameObject leftSpawner;
     public GameObject rightSpawner;
+    public GameObject walls;
 
     float ballOffset;
     float spawnerOffset;
+    float wallsOffset;
     public float lerpRate;
     
 
@@ -20,6 +22,7 @@ public class cameraControl : MonoBehaviour
     void Start()
     {
         spawnerOffset = leftSpawner.transform.position.y - transform.position.y;
+        wallsOffset = walls.transform.position.y - transform.position.y;
         ballOffset = ball.transform.position.y - transform.position.y;
     }
 
@@ -40,6 +43,7 @@ public class cameraControl : MonoBehaviour
             transform.position = new Vector3(transform.position.x, pos, transform.position.z);
             leftSpawner.transform.position = new Vector3(leftSpawner.transform.position.x, pos + spawnerOffset, 0);
             rightSpawner.transform.position = new Vector3(rightSpawner.transform.position.x, pos + spawnerOffset, 0);
+            walls.transform.position = new Vector3(walls.transform.position.x, pos + wallsOffset, 0);
         }
     }
 }
